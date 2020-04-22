81d6f0d4aecc2e6bbda8bf5fa4f2d2e52124c2ec
Tuuletallaja <sanderaru@gmail.com>
2020-04-22 22:15:07 +0300

    working keypress check

--- app.js
+++ app.js
@@ -1,28 +1,50 @@
+var keys = {};
+var questions;
+var question;
+var i = 0
 
 
-var map = {};
+fetch("http://localhost:8000/questions/visual_studio.json")
+.then(response => {
+    return response.json();
+})
+.then(json => {
+    questions = json.questions;
+questions.forEach(x => {
+    i = i+1;
+});
+/* 
+    console.log(questions); */
+    var qnumb = questions.lenght;
+/*     console.log(i); */
+    var index = Math.floor(Math.random() * i);
+    question = questions[index];
+    console.log(question.question);
+    document.getElementById("question").innerHTML = question.question
+})
+
+
 
 onkeydown = onkeyup = function(event) {
 
-    map[event.key] = event.type == 'keydown';
-
-    map2 = JSON.stringify(map);
-    console.log(map2);
-    evaluate();
-
-
-}
-evaluate = function() {
-/*     var combo = JSON.parse(combo);
-    console.log(combo[1]); */
-    var right = ["Control", "f"]
-    if (map != undefined) {
-        
-        
+    keys[event.key] = event.type == 'keydown';
+    
+    for (var key in keys) {
+        if (keys[key] === false) {
+            delete keys[key];
+        }
     }
 
-};
+    var answer = question.answer;
+    console.log(answer);
+    console.log(keys);
+    console.log(_.isEqual(answer,keys));
 
-//document.addEventListener('onkeydown', evaluate);
+    if (_.isEqual(answer,keys)) {
+        var gratz = 'ÕIGE!';
+        document.getElementById("gratz").innerHTML = gratz;
+    }
+
+}
 
 
